fca63f599d3834ca096dce33671404cf3f3bfc5e
nahucham18 <nahu.chamo19@gmail.com>
2023-02-23 12:49:35 -0300

    Redux, Componente Nav

---
+++ fridevi/src/components/Nav/Nav.jsx
@@ -0,0 +1,26 @@
+import style from './Nav.module.css'
+import { Link } from 'react-router-dom'
+
+
+const Nav = () =>{
+    return (
+        <div className={style.container}>
+            
+            <div className={style.menu}>
+            <img className={style.image} src="http://www.fridevi.com.ar/images/master/header/bannerFRIDEVI_2016.png" alt="fridevi" />
+                <Link className={style.link} to={`/nosotros`}><span>Nosotros</span></Link>
+                <Link className={style.link} to={`/noticias`}><span>Nosticias</span></Link>
+                <Link className={style.link} to={`/clientes`}><span>Clientes</span></Link>
+                <Link className={style.link} to={`/contacto`}><span>Contacto</span></Link>
+                <Link className={style.link} to={`/login`}><span>Login</span></Link>
+            </div>
+            <div className={style.lenguaje}>
+                <Link className={style.link} to={`/`}>Español</Link>
+                <Link className={style.link} to={`/`}>English</Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Nav
